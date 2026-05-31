f01930df1341ebbeef0ef63f280eefd6ab9416be
Neagu Robert Alexandru <127115019+NeaguRobertAlexandru31@users.noreply.github.com>
2026-05-31 11:29:58 +0200

    @new(auth) - cognito integration

---
+++ src/app/features/auth/confirm/confirm.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm',
+  imports: [],
+  templateUrl: './confirm.html',
+  styleUrl: './confirm.css',
+})
+export class Confirm {}
